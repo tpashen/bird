fda7f90801d8eabe50be9c4670028cc430f1e4e1
tpashen <81863630+tpashen@users.noreply.github.com>
2021-06-12 15:45:13 +0300

    Update bird.py

--- bird.py
+++ bird.py
@@ -11,6 +11,7 @@ mw.fill(back)
 clock = pygame.time.Clock()
  
 game = True
+ 
 class Area():
     def __init__(self, x=0, y=0, width=10, height=10, color=None):
         self.rect = pygame.Rect(x, y, width, height)
@@ -25,72 +26,85 @@ class Area():
         return self.rect.collidepoint(x, y)      
     def colliderect(self, rect):
         return self.rect.colliderect(rect)
+ 
 class Label(Area):
-    def set_text(self, text, fsize=12, text_color=(0,0,0)):
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
         self.image = pygame.font.SysFont('Arial', fsize).render(text, True, text_color)
     def draw(self, shift_x=0, shift_y=0):
         self.fill()
         mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+ 
 class Picture(Area):
-    def __init__(self, filegame, x = 0, y = 0,width =10, height = 10, angle = 0):
+    def __init__(self, filename, x = 0, y = 0, width = 10, height = 10, angle = 0):
         Area.__init__(self, x=x, y=y, width=width, height=height, color=None)
         self.image = pygame.image.load(filename)
-        self.image = pygame.transform.scale(self.image,(width,height))
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.image = pygame.transform.rotate(self.image, angle)
-
+        
     def draw(self):
         mw.blit(self.image, (self.rect.x, self.rect.y))
-      
+ 
 bird = Picture('bird.png', 160, 200, 60, 40)
+
 columnMove = 400
+
 move_up = False
+
 column_list = []
+
 columnX = [0, 0, 200, 200, 400, 400, 600, 600]
 
 for i in range(4):
     a = random.randint(100, 300)
     columnDownY = widthDisplay - a
-    ColumnUpY = widthDisplay - a - 450
+    columnUpY = widthDisplay - a - 450
     column = Picture('column.png', i*200+500, columnDownY, 100, 300)
     column1 = Picture('column.png', i*200+500, columnUpY, 100, 300, 180)
     column_list.append(column)
     column_list.append(column1)
 
+
 while game:
     mw.fill(back)
-    bird.rect.y -= 4
-
-    for i in range(len(column_list)):
+    bird.rect.y += 4
+    
+    
+    for i in range(len(column_list)): 
+        
         column_list[i].rect.x -= 4
         column_list[i].draw()
         if column_list[i].rect.x <= -100:
             del column_list[1]
             del column_list[0]
-            a = random.rqandint(100, 300)
+            a = random.randint(100, 300)
             columnDownY = widthDisplay - a
-            columnUpY = widthDisplay - a -450
+            columnUpY = widthDisplay - a - 450
             column = Picture('column.png', 700, columnDownY, 100, 300)
             column1 = Picture('column.png', 700, columnUpY, 100, 300, 180)
             column_list.append(column)
-            column_list.append(column1)
-    bird.draw()
+            column_list.append(column1)          
+    bird.draw() 
+    
+    
+    
     for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        game = False
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPASE:
-            move_up = True
-    elif event.type == pygame.KEYUP:
-        if event.key == pygame.K_SPACE:
-            move_up = False
-    if move_up:
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:    
+                move_up = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:    
+                move_up = False       
+                
+    if move_up:   
         bird.rect.y -= 10
-    for i in column_list:
-        if i.rect/colliderect(bird.rect):
+    for i in column_list: 
+        if i.rect.colliderect(bird.rect):
             game = False
     if bird.rect.y > 500 or bird.rect.y < 0:
-        game = False
+        game= False
     pygame.display.update()
-    clock.tick(40)
+    clock.tick(40) 
     
 
