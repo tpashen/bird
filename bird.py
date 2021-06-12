171c983243bc4e44d540f297806617e5f90da7e0
tpashen <81863630+tpashen@users.noreply.github.com>
2021-06-12 15:19:01 +0300

    Update bird.py

--- bird.py
+++ bird.py
@@ -11,6 +11,20 @@ mw.fill(back)
 clock = pygame.time.Clock()
  
 game = True
+class Area():
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.fill_color = back
+        if color:
+            self.fill_color = color
+    def color(self, new_color):
+        self.fill_color = new_color
+    def fill(self):
+        pygame.draw.rect(mw, self.fill_color, self.rect)
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)      
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
 class Label(Area):
     def set_text(self, text, fsize=12, text_color=(0,0,0)):
         self.image = pygame.font.SysFont('Arial', fsize).render(text, True, text_color)
